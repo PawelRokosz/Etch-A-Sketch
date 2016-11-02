4a58c4b196410533a61edf96c13282284fa103c5
Paweł Rokosz <rokosz.pawel@gmail.com>
2016-11-02 19:51:36 +0100

    First functional mvp

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-$("#foo").append("<div>hello world</div>");
+$(document).ready(function() {
+
+  for (i=0; i<256; i++){
+    $(".wrapper").append("<div class='grid'></div>");
+  }
+
+  $(".grid").one("mouseover", function() {
+  $(this).addClass('color');
+});
+});
