3ae3554616d3bb4dfdd78c6cd9aa405a691ef34a
Paweł Rokosz <rokosz.pawel@gmail.com>
2016-11-03 18:13:21 +0100

    working version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-  for (i=0; i<256; i++){
-    $(".wrapper").append("<div class='grid'></div>");
-  }
+  newGrid(16);
 
-  $(".grid").one("mouseover", function() {
-  $(this).addClass('color');
+  $('.grid').mouseover(colored);
+
+  $('#new_grid').on('click', function(){
+    $('.grid').css({'opacity': '0.1'});
+    var numberOfNewSqueres = prompt("How big would you like your new grid was?", 16);
+    if(numberOfNewSqueres !== null){
+      if(numberOfNewSqueres < 1 || numberOfNewSqueres > 100){
+        alert("Given number is out of range! Please try again :)");
+      }else{
+        squereSize = (512/numberOfNewSqueres);
+        newGrid(numberOfNewSqueres);
+        $('.grid').css({'height': squereSize + 'px', 'width': squereSize + 'px'});
+      }
+    }
+    $('.grid').mouseover(colored);
   });
 
+  function newGrid(squere){
+    var numberOfSqueres = squere*squere;
+    $('#wrapper').empty();
+    for(i=0; i<numberOfSqueres; i++){
+      $("#wrapper").append("<div class='grid'></div>");
+    }
+  }
 
+  function colored(){
+    var opacityNow = +$(this).css('opacity');
+    if(opacityNow < 1){
+      opacityNow += 0.1;
+    }else{
+      return false;
+    }
+    $(this).css({'opacity': opacityNow});
+  }
 });
